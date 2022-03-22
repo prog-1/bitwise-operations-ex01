70a482a6407ec48421d81b5368c8620c23929d1c
Littleunidragon <a.voropajeva3@gmail.com>
2022-03-22 19:51:36 +0200

    main done + power of 2

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"math/bits"
+)
+
+func onevar() (a uint64) {
+	fmt.Println("Enter number: ")
+	fmt.Scan(&a)
+	return a
+}
+func twovar() (x uint64, n int) {
+	fmt.Println("Enter number: ")
+	fmt.Scan(&x)
+	fmt.Println("Enter n: ")
+	fmt.Scan(&n)
+	return x, n
+}
+func UI() {
+	for {
+		fmt.Println("Choose program: 1,2,3,4,5,6,7,8,9 or 10* else - exit ")
+		var action int
+		fmt.Scan(&action)
+		if action == 1 {
+			fmt.Println(Even(onevar()))
+		} else if action == 2 {
+			fmt.Println(Odd(onevar()))
+		} else if action == 3 {
+			fmt.Println(SetBit(twovar()))
+		} else if action == 4 {
+			fmt.Println(ClearBit(twovar()))
+		} else if action == 5 {
+			fmt.Println(FlipBit(twovar()))
+		} else if action == 6 {
+			fmt.Println(Ones(onevar()))
+		} else if action == 7 {
+			fmt.Println(Reverse(onevar()))
+		} else if action == 8 {
+			fmt.Println(LeadingZeroes(onevar()))
+		} else if action == 9 {
+			fmt.Println(TrailingZeroes(onevar()))
+		} else if action == 10 {
+			Powerof2()
+		} else {
+			break
+		}
+	}
+}
+func Even(a uint64) bool {
+	if a&1 == 0 {
+		return true
+	}
+	return false
+}
+func Odd(a uint64) bool {
+	if a&1 != 0 {
+		return true
+	}
+	return false
+}
+func SetBit(x uint64, n int) uint64 {
+	return (1 << n) | x
+}
+func ClearBit(x uint64, n int) uint64 {
+	return x &^ (1 << n) //number &= ~(1UL << n);
+}
+func FlipBit(x uint64, n int) uint64 {
+	return x ^ (1 << n)
+}
+func Ones(a uint64) (count int) {
+	for a != 0 {
+		if (a & 1) == 1 {
+			count++
+		}
+		a >>= 1
+	}
+	return count
+	//bits.OnesCount64(a)
+}
+func Reverse(a uint64) uint64 {
+	return bits.ReverseBytes64(a)
+}
+func LeadingZeroes(a uint64) int {
+	return bits.LeadingZeros64(a)
+}
+func TrailingZeroes(a uint64) int {
+	return bits.TrailingZeros64(a)
+}
+func Powerof2() {
+	fmt.Println("The program determines if a number is a power of 2.")
+	var n, i int
+	fmt.Print("Enter the number: ")
+	fmt.Scan(&n)
+	if n > 0 && (n&(n-1)) == 0 {
+		for z := 1; z != n; z *= 2 {
+			i++
+		}
+		fmt.Printf("%v = 2^%v", n, i)
+	} else {
+		fmt.Println(n, "is not a power of 2.")
+	}
+}
+func main() {
+	UI()
+}
